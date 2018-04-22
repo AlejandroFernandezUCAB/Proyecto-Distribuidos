f01455e814000334eef26969307fd2ae0dc2c728
gian <gianfrancoverrocchi@gmail.com>
2018-04-22 18:24:12 -0400

    Agregadas funciones y archivo de configuracion

--- PythonSFTP.py
+++ PythonSFTP.py
@@ -1,18 +1,49 @@
 import pysftp
 
-with pysftp.Connection(
-        '192.168.25.100',
-        #Nombre Usuario
-        username='pafernandez.14', 
-        #Sitio de su clave privada
-        private_key='/home/pedro/Documentos/Universidad/Sistemas Distribuidos/Pedro', 
-        #Passphrase  de su clave privada
-        private_key_pass='informatica.668' 
-        ) as sftp:
 
-    #Aqui se coloca la direccion donde se va a subir el archivo, en este caso somos el grupo 13
-    with sftp.cd('/home/group/distribuidos/201825_25789/13'):
-        #Aqui hay que colocar el archivo a subir
-        sftp.put('HolaMundoFTP')
-        for attr in sftp.listdir_attr():
-            print attr.filename, attr
+# lee las credenciales del archivo de configuracion.
+# Si no lo encuentra las crea...
+def obtenerCredenciales():
+    pkeyPass = ''
+    pkeyPath = ''
+    user = ''
+    try:
+        configFile = open('user.conf','r') 
+        user = configFile.readline().replace('\n','')
+        pkeyPath = str(configFile.readline()).replace('\n','')
+        pkeyPass = str(configFile.readline()).replace('\n','')
+
+    except IOError:
+        print 'No se encontro el archivo de configuracion...creandolo...'
+        user = str(raw_input('Introduzca su usuario UCAB (sin @est.ucab.edu.ve) >> ')) + '\n'
+        pkeyPath = str(raw_input('Introduzca la ruta de su llave privada >> ')) + '\n'
+        pkeyPass = str(raw_input('Introduzca la clave de su llave >> '))
+        configFile = open('user.conf','w')
+        configFile.write(user)
+        configFile.write(pkeyPath)
+        configFile.write(pkeyPass)
+    # print 'usuario {} \nruta {} \nclave {}'.format(user, pkeyPath, pkeyPass)
+    return [user, pkeyPath, pkeyPass]
+
+def conexionSftp(credenciales):
+
+    return pysftp.Connection(
+        '192.168.25.100',
+        username=str(credenciales[0]), 
+        private_key=str(credenciales[1]), 
+        private_key_pass=str(credenciales[2]) 
+        )
+
+    # return pysftp.Connection(
+    #         '192.168.25.100',
+    #         username=user, 
+    #         private_key=pkeyPath, 
+    #         private_key_pass=pkeyPass 
+    #         )
+
+if __name__ == '__main__':
+    credenciales = obtenerCredenciales()
+    print credenciales
+    conexion = conexionSftp(credenciales)
+
+    conexion.mkdir('holaMundo', mode=664)
