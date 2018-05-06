027a8cfc5b7948570733dcc38635e8a18b3ffbdd
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2018-05-06 13:00:37 -0400

    Se ordena correctamente las palabras

--- merge_sort/mergesort.py
+++ merge_sort/mergesort.py
@@ -2,6 +2,7 @@
 
 #Funcion para abrir,leer los archivos y todas las palabras
 #y pasarlas a un array para luego proceder a  ordenarlas
+#Se retorna palabras
 def abrirArchivo( nombreArchivo ):
 
     archivo = open(nombreArchivo, "r")
@@ -11,8 +12,10 @@ def abrirArchivo( nombreArchivo ):
 
 #Con esta funcion se va a agarrar el array2 y se agregara a la cola
 #Del array1, quedando array1 + arra2 (No es una suma)
-#Luego procede a ordenarlo
+#Luego procede a ordenarlo y lo retorno
 def agregarPalabrasAOtroArray( array1 , array2 ):
+    
+    #Agarra el array de segundo lugar y mete elemento por elemento en el array1
     for x in array2:
         array1.append( x )
     array1.sort()
@@ -23,4 +26,9 @@ if __name__ == '__main__':
 
     archivo1 = abrirArchivo("archivo1")
     archivo2 = abrirArchivo("archivo2")
-    imprimir1 = agregarPalabrasAOtroArray( archivo1, archivo2)
+    imprimir = agregarPalabrasAOtroArray( archivo1, archivo2)
+    archivo3 = abrirArchivo("archivo3")
+    imprimir = agregarPalabrasAOtroArray( imprimir, archivo3)
+    archivo4 = abrirArchivo("archivo4")
+    imprimir = agregarPalabrasAOtroArray( imprimir, archivo4)
+    print imprimir
