bf7c5ff5ad2d0fd4d9fb4ac2a6d51cfdd005a701
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2018-05-06 13:23:06 -0400

    Agregando arreglos finales, solo hay un detalle con el archivo1

--- merge_sort/mergesort.py
+++ merge_sort/mergesort.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+#Imprimir Archivo ordenado
+def imprimirArchivo( imprimir ):
+    file = open( "FINAL", "w")
+    for x in imprimir:
+        file.write( x )
+    file.close()
+
 #Funcion para abrir,leer los archivos y todas las palabras
 #y pasarlas a un array para luego proceder a  ordenarlas
 #Se retorna palabras
@@ -8,6 +15,7 @@ def abrirArchivo( nombreArchivo ):
     archivo = open(nombreArchivo, "r")
     palabras = archivo.readlines()
     palabras.sort()
+    archivo.close()
     return palabras
 
 #Con esta funcion se va a agarrar el array2 y se agregara a la cola
@@ -17,7 +25,12 @@ def agregarPalabrasAOtroArray( array1 , array2 ):
     
     #Agarra el array de segundo lugar y mete elemento por elemento en el array1
     for x in array2:
-        array1.append( x )
+        if "\n" not in x: 
+            x = x + "\n"
+            array1.append( x )
+        else:
+            array1.append( x )
+        
     array1.sort()
     return array1
 
@@ -32,3 +45,13 @@ if __name__ == '__main__':
     archivo4 = abrirArchivo("archivo4")
     imprimir = agregarPalabrasAOtroArray( imprimir, archivo4)
     print imprimir
+    imprimirArchivo( imprimir )
+
+#Pasos:
+    # 1. Se procede a abrir el archivo y se retorna en una lista POR LINEA
+    # 2. Se abre el siguiente archivo y se retorna de nuevo linea por linea
+    # 3. Se agarran las 2 listas anteriores y se ordenan alfabeticamente
+    # 4. Se abre un tercer archivo y se retorna linea por linea la lista
+    # 5. Se agarra el array ordenado y se le agrega lo dle tercer archivo 
+    # 6. Asi sucesivamente hasta terminar de leer los archivos
+    # 7. Se imprime el archivo correctamente  
