f975e5b933453d1a33d2888255c8855c0142a216
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2018-05-07 09:37:42 -0400

    Arreglando error de array, pudiera simplicarlo mas

--- merge_sort/mergesort.py
+++ merge_sort/mergesort.py
@@ -12,7 +12,7 @@ def imprimirArchivo( imprimir ):
 #Se retorna palabras
 def abrirArchivo( nombreArchivo ):
 
-    archivo = open(nombreArchivo, "r")
+    archivo = open( nombreArchivo , "r")
     palabras = archivo.readlines()
     palabras.sort()
     archivo.close()
@@ -22,26 +22,36 @@ def abrirArchivo( nombreArchivo ):
 #Del array1, quedando array1 + arra2 (No es una suma)
 #Luego procede a ordenarlo y lo retorno
 def agregarPalabrasAOtroArray( array1 , array2 ):
-    
+    arrayDevolver = []
     #Agarra el array de segundo lugar y mete elemento por elemento en el array1
+    for x in array1:
+        if "\n" not in x: 
+            x = x + "\n"
+            arrayDevolver.append( x )
+        else:
+            arrayDevolver.append( x )
+
     for x in array2:
         if "\n" not in x: 
             x = x + "\n"
-            array1.append( x )
+            arrayDevolver.append( x )
         else:
-            array1.append( x )
+            arrayDevolver.append( x )
         
-    array1.sort()
-    return array1
+    arrayDevolver.sort()
+    return arrayDevolver
 
 #Main
 if __name__ == '__main__':
-
     archivo1 = abrirArchivo("archivo1")
     archivo2 = abrirArchivo("archivo2")
+    #archivo1 = abrirArchivo("/home/pedro/Documentos/Universidad/Sistemas Distribuidos/ProyectoDistribuidos/merge_sort/archivo1")
+    #archivo2 = abrirArchivo("/home/pedro/Documentos/Universidad/Sistemas Distribuidos/ProyectoDistribuidos/merge_sort/archivo2")
     imprimir = agregarPalabrasAOtroArray( archivo1, archivo2)
+    #archivo3 = abrirArchivo("/home/pedro/Documentos/Universidad/Sistemas Distribuidos/ProyectoDistribuidos/merge_sort/archivo3")
     archivo3 = abrirArchivo("archivo3")
     imprimir = agregarPalabrasAOtroArray( imprimir, archivo3)
+    #archivo4 = abrirArchivo("/home/pedro/Documentos/Universidad/Sistemas Distribuidos/ProyectoDistribuidos/merge_sort/archivo4")
     archivo4 = abrirArchivo("archivo4")
     imprimir = agregarPalabrasAOtroArray( imprimir, archivo4)
     print imprimir
