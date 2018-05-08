a52884f61d89846826e30025a65bebefd761ae24
gianfranco <gianfrancoverrocchi@gmail.com>
2018-05-08 18:11:13 -0400

    Creado script para cargar archivo de palabras

---
+++ word_loader/loader.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -()
+mapa = {}
+# diccionario que tiene las palabras y sus definiciones
+diccionario = {}
+
+PATH_LIBRO = 'libro_medicina.txt'
+PATH_DICCIONARIO = '../palabras_libro_medicina.txt'
+
+# ----- /Variables Globales ------
+
+# ----- Funciones Para Leer, Contar y Modificar ------
+
+# lee el archivo PATH_DICCIONARIO
+# inicializa el mapa de palabras
+def cargarDiccionario():
+    with open(PATH_DICCIONARIO,'r') as diccionario_palabras:
+        palabras_diccionario = diccionario_palabras.readlines()
+        for palabra_definicion in palabras_diccionario:
+            temp = palabra_definicion.split('=')
+            palabra = temp[0]
+            definicion = temp[1]
+            mapa[palabra] = [0, None]
+            diccionario[palabra] = eliminarSaltosDeLinea(definicion)
+
+# elimina los saltos de linea
+def eliminarSaltosDeLinea(linea):
+    return linea.replace("\n","")
+
+# source: https://stackoverflow.com/questions/761804/how-do-i-trim-whitespace-from-a-python-string
+def strip_one_space(s):
+    if s.endswith(" "): s = s[:-1]
+    if s.startswith(" "): s = s[1:]
+    return s
+
+def main():
+    cargarDiccionario()
+    print((mapa))
+    print ""
+    print((diccionario))
+
+if __name__ == '__main__':
+    main()
