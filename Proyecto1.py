62eb87bccb8b15598f6b502d12e355d1c021081b
Gianfranco Verrocchi <gianfranco@gmail.com>
2018-05-27 19:18:03 -0400

    Agregando cambios presentes en el cluster

--- Proyecto1.py
+++ Proyecto1.py
@@ -1,21 +1,33 @@
+#!/usr/bin/env python
 from mpi4py import MPI
 import time
 import random
 import sys
+import os
 
 # -*- coding: utf-8 -()
 mapa = {}
 # diccionario que tiene las palabras y sus definiciones
 diccionario = {}
-
-PATH_LIBRO = 'libro_medicina.txt'
-PATH_DICCIONARIO = 'palabras_libro_medicina.txt'
+PATH_LOCAL = '/local_home/'+os.environ['USER']
+PATH_LIBRO = PATH_LOCAL + '/libro_medicina.txt'
+PATH_DICCIONARIO = PATH_LOCAL + '/palabras_libro_medicina.txt'
 
 # ----- /Variables Globales ------
 
 # ----- Funciones Para Leer, Contar y Modificar ------
+def reemplazarPrimeraPalabra(lineas_libro, diccionarioE):
 
-#Funcion para pasar las palabras del diccionario al mapa
+    # recorremos el mapa
+    for palabra in mapa.keys():
+        # si se encontro la palabra
+        if mapa[palabra][0] != 0:
+            lineas_libro[ mapa[palabra][1] ] = lineas_libro[ mapa[palabra][1] ].lower().replace(palabra,diccionarioE[palabra])
+
+def imprimiendoDiccionarioFinal( diccionario ):
+    with open('diccionarioFinalizado.txt', 'w') as diccionarioFinal:
+        for lineaDiccionario in diccionario:
+            diccionarioFinal.writelines( lineaDiccionario.capitalize() + "\n") 
 
 # recibe el diccionario que posee el esclavo
 def inicializarMapa( diccionarioE ):
@@ -27,22 +39,6 @@ def inicializarMapa( diccionarioE ):
 # recibe: linea del libro y su indice respectivo
 # devuleve: un diccionario que tiene la siguiente composicion:
 def contarPalabras(linea, indice):
-    # A D V E R T E C I A
-    # ---------------------
-    # la siguiente linea es un fume
-    # tarde como media hora para resolverlo
-    # muy bonito python...
-    # pero una cagada el manejo de caracteres ;)
-    #	Linea para version aterir de libr ---> linea = linea.decode('cp1252').encode('utf-8')
-    # explicacion:
-        # para obtener el archivo txt use adobe reader para convertir el PDF
-        # al parecer el programa utiliza esa codificacion de caracteres por vainas de windows
-        # https://marketing.adobe.com/resources/help/en_US/whitepapers/multibyte/multibyte_windows1252.html
-        # https://stackoverflow.com/questions/12468179/unicodedecodeerror-utf8-codec-cant-decode-byte-0x9c?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-    
-    # print ('\nlinea: {}'.format(linea))
-    # time.sleep(0.8)
-    # iteramos las llaves del mapa previamente creado
     for palabra in mapa.keys():
         # busca la incidencia de la palabra en toda la linea
         incidencias = linea.count(palabra,0,len(linea))
@@ -59,7 +55,7 @@ def contarPalabras(linea, indice):
 def cargarDiccionario():
     with open(PATH_DICCIONARIO,'r') as diccionario_palabras:
         for line in diccionario_palabras:
-            splitUpLine = line.split(" ",1)
+            splitUpLine = line.lower().split(" ",1)
 
             palabra = splitUpLine[0]
             definicion = splitUpLine[1]
@@ -73,61 +69,48 @@ def cargarDiccionario():
 def limpiarString(linea):
     return linea.replace("\n","").replace('"',"")
 
-# source: https://stackoverflow.com/questions/761804/how-do-i-trim-whitespace-from-a-python-string
-# def strip_one_space(s):
-#     if s.endswith(" "): s = s[:-1]
-#     if s.startswith(" "): s = s[1:]
-#     return s
+
+def extraerYOrdenarPalabrasDeMapa(mapaWorker):
+    result = []
+    for palabra in mapaWorker.keys():
+        auxPalabra = palabra + " "+ str(mapaWorker[palabra][0])
+        stringAEnviar = auxPalabra.lower()
+        result.append( stringAEnviar )
+    result.sort()
+    return result
 
 def main():
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
+    name = MPI.Get_processor_name()
 
     # si eres el coordinador
     if rank == size-1:
         # CARGAR LISTA DE PALABRAS EN MEMORIA
         cargarDiccionario()
-        # print((mapa))
-        # print ""
-        # print((diccionario))
-        #
-        
-        #SCATTER A LOS NODOS
-        # picamos el arreglo
-        
-        
-
-
         palabras = len(diccionario)
         print "Numero de palabras: {}".format(palabras)
-        
-        
 
-
-        # nodos = size 
 
         chunksize = int(round(palabras/float(size)))
         print "palabras por nodo: {}".format(chunksize)
-        # workload = []
+
         messagesSent = []
         print "Proceso Coordinador envia palabras, de manera asincona"
         for i in range(size-1):
             # Envio a todos los Trabajadores
             if i < size-2:
                 temp = {element:diccionario[element] for idx,element in enumerate(diccionario) if (idx)>=chunksize*i and (idx)<chunksize*(1+i)}
-                #temp = palabras[chunksize*i:chunksize*(1+i):]
-                
-                print "coordinador --> voy a enviar: ",len(temp)," a proceso ",i, ". (",chunksize*i,",",chunksize*(1+i),")"
+                print "coordinador --> voy a enviar: ",len(temp)," a proceso ",i, ". (",chunksize*i,",",(chunksize*(1+i))-1,")"
                 # ENVIO SINCRONO F U N C I O N A
                 messagesSent.append(comm.isend(temp,dest=i,tag=99))
                 # Envio a ultimo trabajador
             elif i == size-2:
                 # temp = palabras[chunksize*i::]
                 temp = {element:diccionario[element] for idx,element in enumerate(diccionario) if (idx)>=chunksize*i and (idx)<=len(diccionario)}
-                print "coordinador --> voy a enviar: ",len(temp)," a proceso ",i, ". (",chunksize*i,",",chunksize*(1+i),")"
-	        messagesSent.append(comm.send(temp,dest=i, tag=99))
-                #comm.send("hola soy tu padre", dest=i, tag=99)
+                print "coordinador --> voy a enviar: ",len(temp)," a proceso ",i, ". (",chunksize*i,",",len(diccionario),")"
+                messagesSent.append(comm.send(temp,dest=i, tag=99))
 	        # print "enviando asincrono a ",i
 	
         print "coordinador termino envio asincrono"
@@ -142,8 +125,10 @@ def main():
         
         contador = 0
         nodo = 0
+        listaDiccionarioC = []
         while True:
             if contador == size-1:
+                imprimiendoDiccionarioFinal(listaDiccionarioC)
                 break
             if not iMensajes[nodo].Get_status():
                 # time.sleep(0.1)
@@ -152,10 +137,26 @@ def main():
             else:
                 temp = iMensajes[nodo].wait()
                 if temp != None:
-                    print "coordinador -> recibi: ",temp,". De Proceso: ",nodo
+                    print "coordinador -> recibi: ",len(temp)," elementos De Proceso: ",nodo
+                    
+                    if contador == 0:
+                        listaDiccionarioC.extend( temp )
+                    else:
+                        listaDiccionarioC.extend( temp )
+                        listaDiccionarioC.sort()
+                        
                     contador += 1
-	        nodo = (nodo+1)%size
+ 
+	    nodo = (nodo+1)%size
         print "coordinador -> sali del ciclo"
+
+        libro_modificado = list(comm.recv(source = size-2 ,tag=420))
+
+        print "coordinador -> Recibi Libro Modificado...Escribiendo En archivo..."
+
+        # Linea de prueba
+        with open('libroModificado RESULTATE.txt','w') as target:
+            target.writelines(libro_modificado)
 	
     # si eres trabajador
     else:
@@ -164,17 +165,14 @@ def main():
         print "Proceso",rank," --> recibi: ",len(data)
         
         # time.sleep(random.randint(1,(rank//3)+2))
-<<<<<<< HEAD
         
-=======
-        comm.send('Exito!', dest=size-1, tag=100)
->>>>>>> 92b24c541afd5548cc36912a80e524f6e95f2ac3
         diccionarioE = data 
         inicializarMapa( diccionarioE )
         lineas_libro = []
         
         try:
             with open(PATH_LIBRO,'r') as libro:
+                comienzo = time.time()
                 # convertimos las lineas del libro en una lista de lineas
                 lineas_libro = libro.readlines()
                 print ('Proceso {} ---> Buscando palabras y generando mapa de incidencias...'.format( str(rank) ))
@@ -182,17 +180,50 @@ def main():
                 for idx, linea in enumerate(lineas_libro):
                     # print(idx)
                     contarPalabras(linea.lower(), idx) # convertimos todas las palabras a minusculas...
+                tiempoTranscurrido = time.time() - comienzo
+                print "Soy el proceso:"+ str(rank) + " y transcurrieron " + str(tiempoTranscurrido) + " segundos ejecutando el proceso del libro"
                 #imprimir cantidad de palabras
-                print ( '\n\nProceso ' + str(rank) + ' leyo :\n'+ str(mapa)+"\n\n")
+                #print ( '\n\nProceso ' + str(rank) + ' leyo :\n'+ str(mapa)+"\n\n")
                 
         except:
             print('Hubo un error leyendo el libro: {}'.format(sys.exc_info()[0]))
             exit()
         
-<<<<<<< HEAD
-        comm.send('Exito!', dest=size-1, tag=100)
-=======
->>>>>>> 92b24c541afd5548cc36912a80e524f6e95f2ac3
+
+        listaPalabras = extraerYOrdenarPalabrasDeMapa( mapa )
+
+        comm.isend( listaPalabras , dest=size-1 , tag=100 ) 
+        
+
+        # F A S E  2
+        # 1 - si el rango del trabajador es diferente a 0 (x != 0)
+        if rank != 0:
+            # a - recibe el libro del anterior (Nx-1  -->  Nx)
+            # data = comm.recv(source=MPI.ANY_SOURCE, tag=77)
+            prev_node = int((rank+size-1)%size)
+            libro_modificado = list(comm.recv(source=prev_node,tag=77))
+            sys.stdout.write('Proceso %s en %s...Recibiendo de %s...\n' % (rank,name,prev_node) )
+
+            # b - reemplaza la primera incidencia de todas sus palabras
+            reemplazarPrimeraPalabra(libro_modificado,diccionarioE)     
+            # escribimos los cambios
+            # c - envia  el libro al siguiente (siguiente = (rango + 1)%TamanoAnillo )
+            next_node = int((rank+1)%size)
+            if next_node != size-1:
+                tag = 77
+            else:
+                tag = 420
+            
+            comm.send( libro_modificado , dest=next_node, tag=tag)
+
+        # 2 - si el rango del trabajador es igual a 0 (x == 0)
+        else:
+            # a - reemplaza la primera incidencia de todas sus palabras
+            reemplazarPrimeraPalabra(lineas_libro,diccionarioE)
+            # escribimos los cambios
+            # b - envia  el libro al siguiente
+            next_node = int((rank+1)%size)
+            comm.send( lineas_libro , dest=next_node, tag=77)
 
 if __name__ == '__main__':
     main()
